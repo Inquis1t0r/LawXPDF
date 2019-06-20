92295fb33043cac1a0c60fbd089ccb76d590e34c
inquis1t0r <piotr.kazimierski92@wp.pl>
2019-06-20 14:32:56 +0200

    Initial pdf parsing setup

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using NUnit;
+
+namespace LawXPDF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FileStream fs = new FileStream("Chapter1_Example1.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, fs);
+            doc.Open();
+
+
+
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DirectoryInfo d = new DirectoryInfo(filepath);
+
+            foreach (var file in Directory.GetFiles(filepath, "*.jpg"))
+            {
+                var image = iTextSharp.text.Image.GetInstance(file);
+                doc.Add(image);
+            }
+            doc.Close();
+
+        }
+    }
+}
